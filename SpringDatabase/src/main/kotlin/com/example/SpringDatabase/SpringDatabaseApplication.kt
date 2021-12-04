a2d510584a78a8f1c245c960ce0fb4bdec11fee7
Leron Bergelson <leronbergelson@gmail.com>
2021-12-03 19:30:48 -0500

    Updated SpringDatabaseApplication.kt
    - Added Player ID and Health attributes
    - Added @GetMapping("PlayerData/{PID}") to retrieve data for specific player based off Player ID

--- SpringDatabase/src/main/kotlin/com/example/SpringDatabase/SpringDatabaseApplication.kt
+++ SpringDatabase/src/main/kotlin/com/example/SpringDatabase/SpringDatabaseApplication.kt
@@ -3,9 +3,9 @@ package com.example.SpringDatabase
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.web.bind.annotation.*
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -19,17 +19,31 @@ fun main(args: Array<String>) {
 }
 interface PlayerDataRepository: JpaRepository<playerdata, Long>{
 
+	@Query(value = "Call GetByPID(:PID)", nativeQuery = true)
+	fun GetByPID(@Param("PID")PID: Int): playerdata
 }
 
 @RestController
 @RequestMapping("api")
-class PlayerDataRestController(val PlayerDataRepo: PlayerDataRepository){
+class PlayerDataRestController(val PlayerDataRepo: PlayerDataRepository)
+{
 	@GetMapping("PlayerData")
 	fun GetAllPlayerEntries() = PlayerDataRepo.findAll()
+
+	@GetMapping("PlayerData/{PID}")
+	fun GetPID(@PathVariable(value = "PID") PID: Int) : playerdata
+	{
+		return PlayerDataRepo.GetByPID(PID)
+	}
+
+	@PostMapping("PlayerData")
+	fun SavePlayerData(@RequestBody PlayerData: playerdata){
+		PlayerDataRepo.save(PlayerData)
+	}
 }
 
 @Entity
 class playerdata(
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	val Id: Long = 0, var XCoord: Float = 0.0f, var YCoord: Float = 0.0f, var ZCoord: Float = 0.0f
+	val Id: Long = 0, var isvalid: Boolean = false, var pid: Int = -1, var health: Float = 100.0f, var XCoord: Float = 0.0f, var YCoord: Float = 0.0f, var ZCoord: Float = 0.0f
 )
